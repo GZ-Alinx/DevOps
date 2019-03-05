90fca94de8bdbb4e832d172755cbc026d3a68429
tangwenqiang <tim.tang@daqian520.com>
2019-03-05 18:23:39 +0800

    first commit

---
+++ 基础监控/SSH登录.py
@@ -0,0 +1,24 @@
+import pexpect
+
+def ssh_comand(hsot, user, password, command):
+    """
+    SSH登录执行命令
+    :param hsot:
+    :param user:
+    :param password:
+    :param command:
+    :return:
+    """
+    ssh = pexpect.spawn('ssh -l {} {} {}'.format(user, host, command))
+    i = ssh.expect(['password','continue connecting (yes/no)?'], timeout=8)
+    if i == 0:
+        ssh.sendline(password)
+    if i == 1:
+        ssh.sendline('yes')
+        ssh.expect('[p,P]assword: ')  # Password/password
+        ssh.sendline(password)
+    index = ssh.expect(["$", "#", pexpect.EOF, pexpect.TIMEOUT])  # 此处注意，root用户登录符号为#，普通用户为$
+    if index != 0:
+        print("登录失败！报错内容：{}；{}".format(ssh.before.decode("utf-8"), ssh.after.decode("utf-8")))
+        return False
+    return ssh
