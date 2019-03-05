de24808402e8f3e5549e88d013a8670e421f148c
tangwenqiang <tim.tang@daqian520.com>
2019-03-05 16:57:45 +0800

    first commit

--- 函数基础.py
+++ 函数基础.py
@@ -1,5 +1,18 @@
+#传参函数
+
 def alinx(a):
     re = print("Your Input = %s"%a)
     return re
 
-alinx(100)
+alinx(999)
+
+#*args 可以传入列表、元组作为参数  **kwargs 可以传入字典作为参数
+
+def dl(a,d,*kwargs):
+    kv = a + d
+    print("你输入的值和 = %s"%kv)
+    return kv
+
+Sum = dl(a,d = int(input("Plaese input :")))
+print(Sum)
+
